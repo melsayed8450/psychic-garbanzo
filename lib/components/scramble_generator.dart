3f026bf17f695384634a6519213b96cb3cca0780
youssif gamal <77980007+Tperm-coder@users.noreply.github.com>
2022-07-23 16:59:01 +0200

    Update scramble_generator.dart

--- lib/components/scramble_generator.dart
+++ lib/components/scramble_generator.dart
@@ -3,16 +3,7 @@ import 'package:dio/dio.dart';
 class ScrambleGenerator {
   static Future<String> getScramble() async {
     Response response = await Dio().get(
-      'https://scrambler-api.herokuapp.com/3x3x3',
-      options: Options(
-        headers: {
-          "Access-Control-Allow-Credentials":
-              true, // Required for cookies, authorization headers with HTTPS
-          "Access-Control-Allow-Headers":
-              "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-          "Access-Control-Allow-Methods": "GET"
-        },
-      ),
+      'https://scrambler-api.herokuapp.com/3x3x3'
     );
     return response.data[0];
   }
